900f7aeaf0ac4eab864aa696109aae603f994155
Kasper Rasmussen <kr@frankly.dk>
2017-12-20 11:59:17 +0100

    Skollr, jquery, header, YTpPlayer + en masse leg er tilføjet

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    //STARTER SKOLLR
+    var s = skrollr.init();
+
+
+    //VIDEO HEADER
+    var video_id = $('#background-video').attr('data-id');
+
+    $('#background-video').YTPlayer({
+        fitToBackground: true,
+        videoId: video_id,
+        mute: true,
+        pauseOnScroll: false,
+        playerVars: {
+            modestbranding: 0,
+            autoplay: 1,
+            controls: 0,
+            showinfo: 0,
+            wmode: 'transparent',
+            branding: 0,
+            rel: 0,
+            autohide: 0,
+            origin: window.location.origin
+        },
+        callback: function() {
+        var player = $('#background-video').data('ytPlayer').player;
+            // console.log("callback", player);
+        }
+    });
+
+});
