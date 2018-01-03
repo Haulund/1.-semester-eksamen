53746d9e55ad8f9c56757d747c988e943c5fedd7
Kasper Rasmussen <kr@frankly.dk>
2018-01-03 14:09:30 +0100

    Video added + nav added

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,13 @@ $(document).ready(function() {
     });
 
 });
+
+    /* Open the sidenav */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "50%";
+    }
+
+    /* Close/hide the sidenav */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
