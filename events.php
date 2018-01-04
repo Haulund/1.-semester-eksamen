54b73d4006fb6c82ab7e3f16be2746e9e3daa347
Haulund <steffen.haulund@gmail.com>
2018-01-04 12:30:02 +0100

    Torsdag den 4. jan slut prut for idag

---
+++ events.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Events</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/92410/responsive-youtube.js"></script>
+    <script src="js/skrollr.min.js"></script>
+
+    <script src="js/scripts.js"></script>
+
+
+</head>
+  <body>
+    <!-- HEADER -->
+    <?php include 'header.php'; ?>
+
+  <main>
+    <section>
+      <blockquote>
+          <p>Skal dit næste event være noget helt særligt?</p>
+          <p>Med udgangspunkt i jeres ønsker og med vores indgående kendskab til den gastronomiske verden, hjælper vi jer med at udvikle spændende arrangementer med gastronomi som et ekslusivt omdrejningspunkt.
+Uanset om det gælder en produktlancering, teambuilding i virksomheden eller afholdelse af et netværksarrangement, kan vi stå for ideudvikling og projektledelse og sikrer kvaliteten, så jeres kunder får en hel unik oplevelse .</p>
+          <p>“Tak for et rigtig vellykket arrangement i fredags... alle var ovenud begejstrede, både de gourmet-vante og dem, der ellers var lidt skeptiske ifht. ”det eksperimenterende”...
+Koncept, betjening og arrangement, alt var bare tip-top.”</p>
+          <span>Nicoline Dahlberg, redaktør hos Forlaget Gyldendal</span>
+      </blockquote>
+    </section>
+
+    <section class="textimage">
+        <figure><img src="https://source.unsplash.com/900x1000"></figure>
+        <div class="text purple" data-bottom-top="margin-top: 0px;" data-top-bottom="margin-top: 300px;">
+            <h2>Lorem ipsum dolor</h2>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        </div>
+    </section>
+
+    <section class="textimage image-right">
+        <figure><img src="https://source.unsplash.com/900x800"></figure>
+        <div class="text green" data-bottom-top="margin-top: 0px;" data-top-bottom="margin-top: 100px;">
+            <h2>Lorem ipsum dolor</h2>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        </div>
+    </section>
+  </main>
+
+
+
+  </body>
+</html>
